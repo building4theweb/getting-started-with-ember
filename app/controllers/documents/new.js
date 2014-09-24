523a4d38d7976fa8fe0912d0a03173ef703780ca
Giovanni Collazo <hello@gcollazo.com>
2014-09-24 09:39:15 -0400

    Reset fields after document create

--- app/controllers/documents/new.js
+++ app/controllers/documents/new.js
@@ -17,6 +17,7 @@ export default Ember.Controller.extend({
       });
 
       doc.save().then(function() {
+        self.setProperties({name: '', text: ''});
         self.transitionToRoute('document', doc);
       });
     },
